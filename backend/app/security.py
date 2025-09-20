019ab5372cd36f851c32fc48750c33dfc90a7952
SiriusKoan <me@siriuskoan.one>
2025-09-20 15:13:03 +0800

    fix(backend): passwd salt verify

--- backend/app/security.py
+++ backend/app/security.py
@@ -2,24 +2,19 @@ from datetime import datetime, timedelta
 from typing import Any, Dict
 
 from jose import JWTError, jwt
-from passlib.context import CryptContext
+from passlib.hash import bcrypt
 
 from app.config import settings
-
-# Password hashing context
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     """Verify a password against its hash."""
-    return pwd_context.verify(
-        plain_password, hashed_password, salt=settings.salt.encode()
-    )
+    return bcrypt.verify(plain_password, hashed_password)
 
 
 def get_password_hash(password: str) -> str:
     """Hash a password for storing."""
-    return pwd_context.hash(password, salt=settings.salt.encode())
+    return bcrypt.hash(password, salt=settings.salt.encode())
 
 
 def create_access_token(
