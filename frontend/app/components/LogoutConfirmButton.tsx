3f9152cebcaee23cc4db5807eee16f52a1172456
Kiri487 <kiri48787@gmail.com>
2025-09-21 01:11:09 +0800

    [Style] Update logout page styles

--- frontend/app/components/LogoutConfirmButton.tsx
+++ frontend/app/components/LogoutConfirmButton.tsx
@@ -2,13 +2,12 @@
 import { Button } from "@chakra-ui/react";
 import { logout } from "../actions/auth";
 
-
 export default function LogoutConfirmButton() {
     return (
         // yes no button, if no, go back to previous page
         <form action={logout}>
-            <Button color="white" bg="red.700" type="submit">Yes</Button>
-            <Button color="white" bg="green.700">No</Button>
+            <Button color="white" bg="green.600" margin="0.5rem" borderRadius="lg" fontSize="lg" type="submit">Yes</Button>
+            <Button color="white" bg="red.600" margin="0.5rem" borderRadius="lg" fontSize="lg">No</Button>
         </form>
     )
 }
--- frontend/app/logout/page.tsx
+++ frontend/app/logout/page.tsx
@@ -4,9 +4,9 @@ import LogoutConfirmButton from '../components/LogoutConfirmButton';
 export default async function LogoutPage() {
     // Are you sure you want to logout?
     return (
-        <>
-            <h1 className="text-2xl font-bold mb-4">Are you sure you want to logout?</h1>
+        <main className="flex flex-1 flex-col items-center justify-center space-y-4">
+            <h2 className="text-2xl font-bold mb-4">Are you sure you want to log out?</h2>
             <LogoutConfirmButton />
-        </>
+        </main>
     );
 }
